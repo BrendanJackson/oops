609077f706865d2e6ff1a92f1d49b2adaa75ac9d
Brendan Alexander Jackson <brendan@Brendans-MacBook-Pro.local>
2016-10-22 00:50:29 -0400

    This could be a great app idea but, moving on

--- task.php
+++ task.php
@@ -2,28 +2,64 @@
 
 class Task 	
 {
-	public $title;
-	public $description;
-	public $completed = false;
+	public $time;
+	public $item;
+	// public $title;
+	// public $description;
+	// public $completed = false;
 	// construct methods are called on instantiation of class
-	public function __construct($title, $description)
+	// public function __construct($title, $description)
+	// {
+	// 	$this->title = $title;	
+	// 	$this->description = $description;	
+	// }
+	// // if run completed = true /else false
+	// public function complete() 
+	// {
+	// 	$this->completed = true;
+	// }
+	public function set_time() 
 	{
-		$this->title = $title;	
-		$this->description = $description;	
-	}
-	// if run completed = true /else false
-	public function complete() 
-	{
-		$this->completed = true;
-	}
-}
+		$time_zone = 'America/New_York';
+		$timestamp = time();
+		$DateTime = new DateTime("now", new DateTimeZone($time_zone)); //first argument "must" be a string
+		$DateTime->setTimestamp($timestamp); //adjust the object to correct timestamp
+		$this->time = $DateTime->format('d.m.Y, H:i:s');
 
-$task = new Task("Through me", "how to take your life from 'For me' to 'Through me' "); 
+	}
+
+	public function get_time() 
+	{
+		if ($removed_time <= $checked_time+7days)
+		{
+			echo "This item is expired";
+		} else {
+			echo "This item will expire in" . $time;
+		}
+	}
+
+	public function __construct($item) 
+	{
+		$this->item = $item;
+	}
+	
+
+}
+$task = new Task("produce");
+var_dump($task->item);
+// $task->what_time();
+$task->set_time();
+var_dump($task->time);
+
+
+// $task->set_time_out($what_time);
+// $task = new Task("Through me", "how to take your life from 'For me' to 'Through me' "); 
 // $task2 = new Task("Go to Fusion Corps");
-$task->complete();
-var_dump("Completed Status: " . $task->completed);
-var_dump("Title: " . $task->title);
-var_dump("Description: " . $task->description);
+// $task->complete();
+// var_dump("Completed Status: " . $task->completed);
+// var_dump("Title: " . $task->title);
+// var_dump("Description: " . $task->description);
 
 
 // give Task a title 
+
